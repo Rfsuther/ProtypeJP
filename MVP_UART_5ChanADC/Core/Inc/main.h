9a36088fb56fa61ea3d7f3828c06fa1c3e601ca9
Rob Sutherland <59866164+Rfsuther@users.noreply.github.com>
2023-04-18 16:11:41 +0900

    added ADC and some GPIOs

--- MVP_UART_5ChanADC/Core/Inc/main.h
+++ MVP_UART_5ChanADC/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_Red_Pin GPIO_PIN_14
+#define LED_Red_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
