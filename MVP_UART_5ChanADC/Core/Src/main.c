27582bd71472796d01f0f288e25205b942a67915
Rob Sutherland <59866164+Rfsuther@users.noreply.github.com>
2023-04-21 15:19:36 +0900

    Added Functions for resetting Structs

--- MVP_UART_5ChanADC/Core/Src/main.c
+++ MVP_UART_5ChanADC/Core/Src/main.c
@@ -59,7 +59,6 @@ TIM_HandleTypeDef htim1;
 UART_HandleTypeDef huart3;
 DMA_HandleTypeDef hdma_usart3_rx;
 
-
 /* USER CODE BEGIN PV */
 struct exoCounter
 {
@@ -106,17 +105,11 @@ struct errorCode
 };
 
 
-
-
 //ADC PVs
 volatile uint16_t Adc1Results[numOfChansADC1];
 volatile uint16_t ADC2_val = 0;
 
 
-ADC_HandleTypeDef hadc1;
-ADC_HandleTypeDef hadc2;
-DMA_HandleTypeDef hdma_adc1;
-TIM_HandleTypeDef htim1;
 
 //UART PVs
 uint8_t txBuffer[txBuffSize] = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '\r', '\n' };
@@ -144,6 +137,12 @@ static void MX_TIM1_Init(void);
 static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
+//Three reset functions for createing structs
+struct exoState ResetexoState(void);
+struct errorCode ReseterrorCode(void);
+struct exoCounter ResetexoCounter(void);
+struct exoThresh ResetexoTresh(void);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -159,40 +158,11 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 	
-	//Initialize EXO Structs
-	struct exoCounter myExoCounter;
-	myExoCounter.LLHits = 0;
-	myExoCounter.LUHits = 0;
-	myExoCounter.RLHits  = 0;
-	myExoCounter.RUHits = 0;
-	myExoCounter.BdyHits = 0;
-	myExoCounter.HdHits = 0;
-	
-	// Set Baseline by adjustment
-	struct exoThresh myExoThresh;
-	myExoThresh.LLThresh = 0;
-	myExoThresh.LUThresh = 0;
-	myExoThresh.RLThresh  = 0;
-	myExoThresh.RUThresh = 0;
-	myExoThresh.BdyThresh = 0;
-	myExoThresh.HdThresh = 0;
-	
-
-	struct exoState myExoState;
-	myExoState.LLStateCounter = 0;
-	myExoState.LUStateCounter = 0;
-	myExoState.RLStateCounter = 0;
-	myExoState.RUStateCounter = 0;
-	myExoState.BdyStateCounter = 0;
-	myExoState.HdStateCounter = 0;
-	
-	myExoState.LLCurrentState = 0;
-	myExoState.LUCurrentState = 0;
-	myExoState.RLCurrentState = 0;
-	myExoState.RUCurrentState = 0;
-	myExoState.BdyCurrentState = 0;
-	myExoState.HdCurrentState = 0;
-		
+	//Initialize EXO Structs ( should be 4)
+	struct exoCounter myExoCounter = ResetexoCounter();
+	struct exoThresh myExoThresh = ResetexoTresh(); // Set Baseline by adjustment
+	struct exoState myExoState = ResetexoState();
+	struct errorCode myErrorCode = ReseterrorCode();
 	
 	
   /* USER CODE END 1 */
@@ -246,68 +216,50 @@ int main(void)
   {
 	  //Update State of Robot
 	  //Reset Condition
-//	  if (PRESSEDBUTTON == 1)
-//	  {
-//		  myExoCounter.LLHits = 0;
-//		  myExoCounter.LUHits = 0;
-//		  myExoCounter.RLHits  = 0;
-//		  myExoCounter.RUHits = 0;
-//		  myExoCounter.BdyHits = 0;
-//		  myExoCounter.HdHits = 0;
-//		  HAL_Delay(500);
-//	  }
-//	  else if(ANY part in error state)
-//	  {
-//		  
-//	  }
-//	  else //check State of punches
-//	  {
-//		  //LL handeler
-//		  if ((myExoState.LLCurrentState == 1) && (myExoState.LLStateCounter == 0))
-//		  {
-//			  //
-//			  myExoCounter.LLHits += 1;
-//			  myExoState.LLStateCounter += 1; 
-//		  }
-//		  //Entered Error State Must Reset System
-//		  else if(myExoState.LLStateCounter > sensrErrCntThresh)
-//		  {
-//			  myExoState.LLStateCounter = -1;			  
-//		  }
-//	  }
-//			  
-//			  
-//			  
-//			  
-//		  myExoState.LLStateCounter = 0;
-//		  myExoState.LUStateCounter = 0;
-//		  myExoState.RLStateCounter = 0;
-//		  myExoState.RUStateCounter = 0;
-//		  myExoState.BdyStateCounter = 0;
-//		  myExoState.HdStateCounter = 0;
-//		  myExoState.LLCurrentState = 0;
-//		  myExoState.LUCurrentState = 0;
-//		  myExoState.RLCurrentState = 0;
-//		  myExoState.RUCurrentState = 0;
-//		  myExoState.BdyCurrentState = 0;
-//		  myExoState.HdCurrentState = 0;
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//			  
-//	  }
-//			
+	  if (PRESSEDBUTTON == 1)
+	  {
+		  //reset exoCounter and state
+		  myExoState = ResetexoState();
+		  myExoCounter = ResetexoCounter();
+		  HAL_Delay(500);
+	  }
+	  else if(ANY part in error state)
+	  {
+		  
+	  }
+	  else //check State of punches
+	  {
+		  //LL handeler
+		  if ((myExoState.LLCurrentState == 1) && (myExoState.LLStateCounter == 0))
+		  {
+			  //
+			  myExoCounter.LLHits += 1;
+			  myExoState.LLStateCounter += 1; 
+		  }
+		  //Entered Error State Must Reset System
+		  else if(myExoState.LLStateCounter > sensrErrCntThresh)
+		  {
+			  myExoState.LLStateCounter = -1;			  
+		  }
+	  }
+			  
+			  
+			  
+			  
+		  myExoState.LLStateCounter = 0;
+		  myExoState.LUStateCounter = 0;
+		  myExoState.RLStateCounter = 0;
+		  myExoState.RUStateCounter = 0;
+		  myExoState.BdyStateCounter = 0;
+		  myExoState.HdStateCounter = 0;
+		  myExoState.LLCurrentState = 0;
+		  myExoState.LUCurrentState = 0;
+		  myExoState.RLCurrentState = 0;
+		  myExoState.RUCurrentState = 0;
+		  myExoState.BdyCurrentState = 0;
+		  myExoState.HdCurrentState = 0;
+	  }
+			
 //	  
 	  HAL_Delay(500);
 	  txStatus = HAL_UART_Transmit(&huart3, txBuffer, sizeof(txBuffer) / sizeof(txBuffer[0]), 100);
@@ -657,20 +609,75 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, LED_Green_Pin|LED_Red_Pin|LED_Blue_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PE2 PE3 PE4 PE5
+                           PE6 PE7 PE8 PE10
+                           PE11 PE12 PE13 PE14
+                           PE15 PE0 PE1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
+                          |GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_10
+                          |GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
+                          |GPIO_PIN_15|GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PushB1_Pin */
   GPIO_InitStruct.Pin = PushB1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(PushB1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC14 PC15 PC1 PC2
+                           PC4 PC5 PC6 PC7
+                           PC8 PC9 PC10 PC11
+                           PC12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_1|GPIO_PIN_2
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
+                          |GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PF0 PF1 PF2 PF3
+                           PF4 PF5 PF6 PF7
+                           PF8 PF9 PF10 PF11
+                           PF12 PF13 PF14 PF15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
+                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PH0 PH1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA0 PA1 PA2 PA4
+                           PA8 PA9 PA10 PA11
+                           PA12 PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_4
+                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
+                          |GPIO_PIN_12|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LED_Green_Pin LED_Red_Pin LED_Blue_Pin */
   GPIO_InitStruct.Pin = LED_Green_Pin|LED_Red_Pin|LED_Blue_Pin;
@@ -679,9 +686,98 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : PB2 PB10 PB11 PB12
+                           PB13 PB15 PB3 PB4
+                           PB5 PB6 PB8 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12
+                          |GPIO_PIN_13|GPIO_PIN_15|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PG0 PG1 PG2 PG3
+                           PG4 PG5 PG6 PG7
+                           PG8 PG9 PG10 PG11
+                           PG12 PG13 PG14 PG15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
+                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PD10 PD11 PD12 PD13
+                           PD14 PD15 PD0 PD1
+                           PD2 PD3 PD4 PD5
+                           PD6 PD7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13
+                          |GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_0|GPIO_PIN_1
+                          |GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
+                          |GPIO_PIN_6|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
+//functions
+
+
+struct exoState ResetexoState(void)
+{
+	struct exoState inStruct;
+	inStruct.LLStateCounter = 0;
+	inStruct.LUStateCounter = 0;
+	inStruct.RLStateCounter = 0;
+	inStruct.RUStateCounter = 0;
+	inStruct.BdyStateCounter = 0;
+	inStruct.HdStateCounter = 0;
+	
+	inStruct.LLCurrentState = 0;
+	inStruct.LUCurrentState = 0;
+	inStruct.RLCurrentState = 0;
+	inStruct.RUCurrentState = 0;
+	inStruct.BdyCurrentState = 0;
+	inStruct.HdCurrentState = 0;
+	return inStruct;
+};
+struct errorCode ReseterrorCode(void)
+{ 
+	
+};
+struct exoCounter ResetexoCounter(void) { 
+	struct exoCounter inStruct;
+	inStruct.LLHits = 0;
+	inStruct.LUHits = 0;
+	inStruct.RLHits  = 0;
+	inStruct.RUHits = 0;
+	inStruct.BdyHits = 0;
+	inStruct.HdHits = 0;
+	return inStruct;
+}
+struct exoThresh ResetexoTresh(void)
+{
+	struct exoThresh inStruct;
+	inStruct.LLThresh = 0;
+	inStruct.LUThresh = 0;
+	inStruct.RLThresh  = 0;
+	inStruct.RUThresh = 0;
+	inStruct.BdyThresh = 0;
+	inStruct.HdThresh = 0;
+	return inStruct;
+}
+
+
+
+
+
+
+
+
+//Modified HAL functions
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
 	asm("nop");
